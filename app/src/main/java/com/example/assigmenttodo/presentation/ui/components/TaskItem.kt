b20fd54f1af085ff1ab2e839ed81d0df1f8e034a
Mohd Saif Ahmed <saifahmed9308@gmail.com>
2025-03-27 11:51:49 +0530

    UI REFACTOR DONE

--- app/src/main/java/com/example/assigmenttodo/presentation/ui/components/TaskItem.kt
+++ app/src/main/java/com/example/assigmenttodo/presentation/ui/components/TaskItem.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
@@ -15,8 +16,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
 import com.example.assigmenttodo.data.model.Task
 
@@ -34,11 +37,13 @@ fun TaskItem(task: Task) {
                     .fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Column {
+                Column(modifier = Modifier.weight(0.8f)) {
                     Text(
                         text = task.title ?: "NO DATA",
                         color = if (task.completed == true) Color.LightGray else Color.White,
-                        fontSize = 16.sp
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        fontSize = 16.sp,
                     )
                     Text(
                         text = "User ID: ${task.userId}",
